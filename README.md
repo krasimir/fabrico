a251f112fcd7b06d7b2205b37e9299471f32c0cb
Krasimir Tsonev <krasimir@outset.ws>
2012-10-18 15:44:19 +0300

    Reconstruction. The idea of the package manager is put on hold for now

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 PHP Micro Framework
+===================
--- examples/basic/index.php
+++ examples/basic/index.php
@@ -1,22 +1,29 @@
 <?php
 
-    require_once(dirname(__FILE__)."/../../src/fabrico/Fabrico.php");
+    require_once(dirname(__FILE__)."/../../src/Fabrico.php");
 
-    $fabrico = new Fabrico();
+    class Application extends Fabrico {
+        public function __construct() {
+            parent::__construct();
+            
+        }
+    }
+
+    new Application();
     
-    $router = new Router();
-    $router->register("/mypage/articles/@articleId/@commentId/@username", function($params) {
-            var_dump($params);
-        })
-        ->register("/mypage/articles/@articleId/@commentId", function($params) {
-            var_dump($params);
-        }, "GET,POST")
-        ->register("/", function($params) {
-            die(view("template.html", array(
-                "titleOfThePage" => "Fabrico(2)",
-                "title" => "Fabrico(2) - PHP Micro Framework"
-            ), dirname(__FILE__)."/tpl/"));
-        })
-        ->run();
+    // $router = new Router();
+    // $router->register("/mypage/articles/@articleId/@commentId/@username", function($params) {
+    //         var_dump($params);
+    //     })
+    //     ->register("/mypage/articles/@articleId/@commentId", function($params) {
+    //         var_dump($params);
+    //     }, "GET,POST")
+    //     ->register("/", function($params) {
+    //         die(view("template.html", array(
+    //             "titleOfThePage" => "Fabrico(2)",
+    //             "title" => "Fabrico(2) - PHP Micro Framework"
+    //         ), dirname(__FILE__)."/tpl/"));
+    //     })
+    //     ->run();
  
 ?>
