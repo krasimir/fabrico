8d900759ba34493cb793a90effb03e05fcf408b0
Krasimir Tsonev <krasimir@outset.ws>
2012-11-21 09:10:54 +0200

    Reconstruction

--- tests/packagemanager/index.php
+++ tests/packagemanager/index.php
@@ -2,7 +2,9 @@
 
     require(dirname(__FILE__)."/../src/fabrico.php");
 
-    $F->loadModule("ErrorHandler", "Router", "View");
+    $F->loadModule("ErrorHandler", "Router", "View", "TestWidget");
     var_dump($F->loaded());
+
+
 
 ?>
