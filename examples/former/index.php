845403993fc1128ab7a177eabd3eb5a003016da1
Krasimir Tsonev <krasimir@outset.ws>
2012-12-19 17:11:10 +0200

    Adding Former module

---
+++ examples/former/index.php
@@ -0,0 +1,88 @@
+<link rel="stylesheet" type="text/css" href="../../src/core/Former/css/styles.css" />
+<div style="width: 400px;">
+<?php
+
+    require("../../src/core/Former/index.php");
+
+    // setting templates path
+    // Former::templatesPath(__DIR__."/");
+
+    // setting custom error message
+    FormerValidation::$MESSAGE_NotEmpty = "Don't leave this field empty!";
+
+    // registering the form
+    $form = Former::register("register-user", "/examples/former/", "GET");
+    $form->addTextBox(array(
+        "name" => "username", 
+        "label" => "Your name:", 
+        "validation" => Former::validation()->NotEmpty()->LengthMoreThen(5)->String()
+    ))
+    ->addTextArea(array(
+        "name" => "description", 
+        "label" => "Few words about you:"
+    ))
+    ->addPasswordBox(array(
+        "name" => "password", 
+        "label" => "Your password:", 
+        "validation" => Former::validation()->NotEmpty()->LengthMoreThen(5)
+    ))
+    ->addTextBox(array(
+        "name" => "salary", 
+        "label" => "Your prefered salary:", 
+        "validation" => Former::validation()->NotEmpty()->LengthMoreThen(3)->Int()->LessThen(1450)
+    ))
+    ->addDropDown(array(
+        "name" => "city",
+        "label" => "Your city:",
+        "options" => array(
+            "none" => "None",
+            "new-york" => "New York",
+            "london" => "London",
+            "paris" => "Paris"
+        )
+    ))
+    ->addRadio(array(
+        "name" => "job",
+        "label" => "Your job:",
+        "options" => array(
+            "none" => "None",
+            "front-end" => "Front-end developer",
+            "back-end" => "Back-end developer"
+        ),
+        "validation" => Former::validation()->Not("none")
+    ))
+    ->addCheck(array(
+        "name" => "special-wishes",
+        "label" => "Special wishes:",
+        "options" => array(
+            "w1" => "fresh water",
+            "w2" => "fruits",
+            "w3" => "dentist"
+        )
+    ))
+    ->addFile(array(
+        "name" => "avatar",
+        "label" => "Please choose your avatar:"
+    ));
+
+    // then in your controller
+    $loginForm = Former::get("register-user", array("description" => "...", "job" => "front-end"));
+    if($loginForm->submitted) {
+        // Form is submitted
+        if($loginForm->success) {
+            // the form is submitted successfully
+            $data = $loginForm->data;
+            var_dump($data);
+        } else {
+            // The form is submitted, but doesn't pass the validation. Show it again.
+            $markup = $loginForm->markup;
+            echo $markup;
+        }
+    } else {
+        // The form is still not submitted. Simply show its markup
+        $markup = $loginForm->markup;
+        echo $markup;
+    }
+
+?>
+</div>
