e2299a6fc2058951e3b3ecac80226c59f13405fa
Krasimir Tsonev <krasimir@outset.ws>
2013-01-12 12:09:29 +0200

    Reconstructing

--- examples/simpleapp/index.php
+++ examples/simpleapp/index.php
@@ -1,6 +1,6 @@
 <?php
 
-    require(__DIR__."/../../lib/fabrico.php");
+    require(__DIR__."/../../lib/Loader.php");
 
     $F->loadModule("Router", "View", "TestWidget");
     $F->loadResource("resources/*", "utils/ErrorHandler/index.php");
